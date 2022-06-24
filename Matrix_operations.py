fce2de67d349c1a496e6975f709d5a3a80d2bc43
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2022-06-24 15:36:35 -0400

    Added matrix funcitons

---
+++ Matrix_operations.py
@@ -0,0 +1,11 @@
+#this file contains the matrix function necessary for learning
+
+def matrix_subtract(matrix1, matrix2):
+    '''Input: accepts two matrices, assumes them to be the same size
+    Output: returns the result of matrix subtraction'''
+    return [[matrix1[j][i] - matrix2[j][i] for i in range(len(matrix1))] for j in range(len(matrix1[0]))]
+
+def matrix_dot_prod(matrix, scalar):
+    '''Input: accepts one matrix and one scalar value
+    Output: returns the result of matrix dot product'''
+    return [[element * scalar for element in row ] for row in matrix]
