f2dd2b97347b8d43b5188fbf5d46b53a80136376
hargunmujral <83234565+hargunmujral@users.noreply.github.com>
2022-07-07 21:04:28 -0700

    added a matrix transpose function

--- Matrix_operations.py
+++ Matrix_operations.py
@@ -9,3 +9,8 @@ def matrix_dot_prod(matrix, scalar):
     '''Input: accepts one matrix and one scalar value
     Output: returns the result of matrix dot product'''
     return [[element * scalar for element in row ] for row in matrix]
+
+def matrix_transpose(matrix):
+    '''Input: accepts a matrix
+    Output: returns the transpose of the matrix'''
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
